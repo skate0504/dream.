cfff18c97309d76e616d3f4889569128824ceb55
skate0504 <skate0504@naver.com>
2025-06-30 14:58:11 +0900

    Add files via upload

---
+++ dreamers_official_site/bundle.js
@@ -0,0 +1,16 @@
+
+const root = document.getElementById("root");
+root.innerHTML = `
+  <div class='container'>
+    <h1>Dreamers Inc Korea Launches Blockchain Security Development Initiative</h1>
+    <p><em>June 30, 2025 · Official Press Release</em></p>
+    <img src="ceo.jpg" alt="CEO KyongTae Cho" />
+    <p>Dreamers Inc, a global tech consulting and R&D company founded in Los Angeles in 2006, has officially announced the launch of its
+    <strong> blockchain security development initiative </strong> through its Korean branch, led by CEO KyongTae Cho...</p>
+    <p>Notably, Dreamers Inc Korea has been <strong>selected as a key technical partner</strong> for the South Korean government’s national initiative...</p>
+    <h2>Contact</h2>
+    <p>Dreamers Inc Korea Public Relations Team<br>
+    📧 press@thedreamers.io<br>
+    🌐 www.thedreamers.io</p>
+  </div>
+`;
